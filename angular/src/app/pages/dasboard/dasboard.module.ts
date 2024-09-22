a06f0f8b30da622a78bf83a57d3ae836eb075895
marlontorres17 <marlontorresmedina@gmail.com>
2024-09-22 18:40:39 -0500

    borrar

--- angular/src/app/pages/dasboard/dasboard.module.ts
+++
@@ -1,7 +0,0 @@
-import { NgModule } from '@angular/core';
-import { DashboardRoutingModule } from './dasboard-routing.module';
-
-@NgModule({
-  imports: [DashboardRoutingModule],
-})
-export class DashboardModule {}
